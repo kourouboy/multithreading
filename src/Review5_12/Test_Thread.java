4dec1f7224aa1c0528cf3d1f73883965084af11b
kourouboy <1376483126@qq.com>
2019-05-12 21:53:49 +0800

    volatile

---
+++ src/Review5_12/Test_Thread.java
@@ -0,0 +1,32 @@
+package Review5_12;
+
+
+class Mythread extends Thread{//线程主体类
+    private String title;
+    public Mythread(String title){
+        this.title = title;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(this.title + ", i = " + i);
+        }
+    }
+}
+public class Test_Thread {
+    public static void main(String[] args) {
+        Mythread mythread = new Mythread("thread1");
+        Mythread mythread1 = new Mythread("thread2");
+        Mythread mythread2 = new Mythread("thread3");
+        //直接调用run()方法，这个时候只是做了一个顺序打印，和多线程一点关系都没有
+
+//       mythread.run();
+//       mythread1.run();
+//       mythread2.run();
+        //正确启用多线程的方式是调用Thread类中的start()方法
+        mythread.start();
+        mythread1.start();
+        mythread2.start();
+    }
+}
